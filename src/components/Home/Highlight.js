f517ef3f92571425c5c03c15438c58bcea8c900a
RDjarbeng <richarddjarbeng@gmail.com>
2020-11-10 16:24:47 +0000

    cms config

--- src/components/Home/Highlight.js
+++ src/components/Home/Highlight.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import parse from 'html-react-parser'
 export default function Highlight({ style, heading, paragraph, color, data_aos }) {
-    console.log(paragraph);
     return(
         <div className="col-md-4 p-3">
             <div className=" shadow col h-100" style={style} data-aos={data_aos}> 
