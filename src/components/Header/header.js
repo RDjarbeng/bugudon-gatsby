55ed71fba319867614ec791c621d0793ddab3809
RDjarbeng <richarddjarbeng@gmail.com>
2020-11-18 23:09:41 +0000

    modified nav link padding on toggle, changed nav to react bootstrap implementation

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,36 +1,47 @@
 import React from 'react';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
+
 import { Link } from 'gatsby';
 
 
 export default function Header() {
-    return(
-        <header id="nav1" style={{height:40}}>
-            <nav className="navbar navbar-expand-lg navbar-light bg-white p-0 nav-menu shadow">
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        return (
+                <header id="nav1" style={{ height: 40 }}>
+                        <Navbar expand="lg" bg="light" variant="light" collapseOnSelect
+                                className="navbar navbar-expand-lg navbar-light bg-white p-0 nav-menu shadow"
+                        >
+                                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <Link className="navbar-brand p-0 mr-auto" to="/"> <img src="/img/logo.jpg" alt="logo" className="logo-img d-inline-block align-top"/></Link>
-                    <ul className="navbar-nav  mt-2 mt-lg-0 mr-5" id="responsive-navbar-nav">
-                    <li className="nav-item">
-                            <Link activeClassName="active" to="/">Home</Link>
-                    </li>
-                    <li className="nav-item">
+                </button> */}
+                                {/* <div className="collapse navbar-collapse" id="navbarTogglerDemo01"> */}
+                                <Navbar.Collapse id="responsive-navbar-nav">
+                                        <Navbar.Brand >
+                                                <Link className="navbar-brand p-0
+                                                 mr-auto" to="/"> <img src="/img/logo.jpg" alt="logo" className="logo-img d-inline-block align-top
+                                                 " /></Link>
+                                        </Navbar.Brand>
+                                        <Nav className="navbar-nav  mt-2 mt-lg-0 mr-xl-5 ml-xl-auto">
+                    <Nav.Link className="nav-item">
+                            <Link activeClassName="active" to="/">Home</Link></Nav.Link>
+                    <Nav.Link className="nav-item">
                             <Link to="/projects" activeClassName="active">Projects</Link>
-                    </li>
-                    <li className="nav-item">
+                    </Nav.Link>
+                    <Nav.Link className="nav-item">
                             <Link to="/investors" activeClassName="active">Investors {'&'} Off takers</Link>
-                    </li>
-                    <li className="nav-item">
+                    </Nav.Link>
+                    <Nav.Link className="nav-item">
                             <Link to="/company" activeClassName="active">Company</Link>
-                    </li>
-                    <li className="nav-item">
+                    </Nav.Link>
+                    <Nav.Link className="nav-item">
                             <Link to="/contact" activeClassName="active">Contact Us</Link>
-                    </li>
-                    </ul>
-                </div>
-                </nav>
-        </header>
-       
-    );
+                    </Nav.Link>
+                    </Nav>
+                                </Navbar.Collapse>
+                {/* </div> */}
+                </Navbar>
+                
+                </header>
+
+        );
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Header from './Header/header';
 import Footer from './Footer/Footer';
 //CSS styles
-import './App.css';
+
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 // import AOS from "aos";//trying to implement aos
 
 
